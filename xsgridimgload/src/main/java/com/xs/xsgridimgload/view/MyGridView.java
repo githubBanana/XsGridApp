eda06c79580a95d33eec4ca3dd29058d411faa03
siushen <784843867@qq.com>
2016-03-28 14:45:55 +0800

    msg: 自定义GridView: MyGridView(解决GridView 不能显示多行的问题)

---
+++ xsgridimgload/src/main/java/com/xs/xsgridimgload/view/MyGridView.java
@@ -0,0 +1,29 @@
+package com.xs.xsgridimgload.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * @version V1.0 <描述当前版本功能>
+ * @author: Xs
+ * @date: 2016-03-27 16:58
+ */
+public class MyGridView extends GridView {
+    public MyGridView(Context context) {
+        super(context);
+    }
+
+    public MyGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyGridView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(536870911, -2147483648);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
